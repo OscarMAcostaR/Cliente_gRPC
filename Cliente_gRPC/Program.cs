680421e0232f6811b33389edc1c2017ba4cfbdf4
Oscar Manuel Acosta Rios <oscar96261@gmail.com>
2024-12-31 14:06:31 -0600

    Agregar archivos de proyecto.

---
+++ Cliente_gRPC/Program.cs
@@ -0,0 +1,32 @@
+﻿using Cliente_gRPC;
+using Grpc.Net.Client;
+
+
+
+using (var channel = GrpcChannel.ForAddress("http://localhost:5057"))
+{
+	var client = new Names.NamesClient(channel);
+
+	// Modo asincrono awaut 
+	var reply = await client.SayHelloAsync(new HelloRequest
+	{
+		Name = "Allstar ",
+	});
+
+	//Respuesta 2 
+
+	var reply2 = await client.GetAsync(new NamesRequest { }); 
+
+	Console.WriteLine(reply.Message);
+
+	//Hacemos un recorrido de los nombre de la respuesta 2
+	foreach ( var name in reply2.Name)
+	{
+		Console.WriteLine(name);
+	}
+
+
+
+	Console.ReadKey(); 
+
+}
